6dde84fdb714ee4302c331e8c28980e43ecc9f6e
Tomasz Dopierala <tomasz@dopierala.net.pl>
2019-02-19 17:56:50 +0000

    New test cases for cells

---
+++ tests/TestCase/View/Cell/SidebarCellTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Test\TestCase\View\Cell;
+
+use App\View\Cell\SidebarCell;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\View\Cell\SidebarCell Test Case
+ */
+class SidebarCellTest extends TestCase
+{
+
+    /**
+     * Request mock
+     *
+     * @var \Cake\Http\ServerRequest|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $request;
+
+    /**
+     * Response mock
+     *
+     * @var \Cake\Http\Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $response;
+
+    /**
+     * Test subject
+     *
+     * @var \App\View\Cell\SidebarCell
+     */
+    public $Sidebar;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->request = $this->getMockBuilder('Cake\Http\ServerRequest')->getMock();
+        $this->response = $this->getMockBuilder('Cake\Http\Response')->getMock();
+        $this->Sidebar = new SidebarCell($this->request, $this->response);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Sidebar);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test display method
+     *
+     * @return void
+     */
+    public function testDisplay()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
